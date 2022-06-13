7e1baeea9f31815783d9d084d0af3427339d34f8
ryan <ryancruse1807@gmail.com>
2022-06-13 17:27:03 +0100

    Project Solution

--- main.js
+++ main.js
@@ -1,3 +1,53 @@
 $(document).ready(() => {
 	
+
+    function handleNavMenu(){
+
+        const $navMenu = $('.nav-menu');
+        const $menu = $('.menu');
+
+        $menu.on('mouseover', () =>{
+            $navMenu.show();
+        });
+    
+        $navMenu.on('mouseleave', () =>{
+            $navMenu.hide();
+        });
+    }
+
+    function buttonHover(){
+        const $btn = $('.btn');
+
+        $btn.on('mouseover', (event) =>{
+            $(event.currentTarget).addClass('btn-hover');
+        }).on('mouseleave', (event) =>{
+            $(event.currentTarget).removeClass('btn-hover');
+        });
+    }
+
+    function limitWordCount(){
+        const $postText = $('.postText');
+        
+        $postText.on('keyup', (event) =>{
+            const post = $(event.currentTarget).val();
+            const charactersRemaining = 140 - post.length;
+            const $wordCount = $('.wordcount');
+
+            if(charactersRemaining <=0){
+                $wordCount.addClass('red');
+                
+            }
+            else{
+                $wordCount.removeClass('red');
+            }
+
+            $postText.focus();
+            $('.characters').html(charactersRemaining);
+        })
+    }
+
+    handleNavMenu();
+    buttonHover();
+    limitWordCount()
+
 }); 
